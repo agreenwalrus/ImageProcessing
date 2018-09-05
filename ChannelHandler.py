8edc6ba78b8abbf2a87121648145e20e5b04437a
Julia Runova <jul.runova@gmail.com>
2018-09-05 18:16:04 +0300

    histogram, negative

--- ChannelHandler.py
+++ ChannelHandler.py
@@ -1,10 +1,23 @@
+import numpy as np
+
 class ChannelHandler:
 
+    def __init__(self, range_of_brigntness=256):
+        self.range_of_brightness = range_of_brigntness
+
     def get_histogram(self, channel):
-        pass
+        histogram = np.zeros(self.range_of_brightness)
+        for brightness in channel:
+            histogram[brightness] += 1
+
+        return histogram
 
     def get_negative(self, channel):
-        pass
+        negative = list()
+        for brightness in channel:
+            negative.append(self.range_of_brightness - brightness)
+
+        return negative
 
     def get_handled_by_harmonic_mean_filter(self, channel):
         pass
