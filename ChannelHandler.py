f86ad6103ce5569f093ee7f02c1c0df6fce1e7dc
Julia Runova <jul.runova@gmail.com>
2018-09-05 18:00:16 +0300

    class ChannelHandler

---
+++ ChannelHandler.py
@@ -0,0 +1,18 @@
+class ChannelHandler:
+
+    def get_histogram(self, channel):
+        pass
+
+    def get_negative(self, channel):
+        pass
+
+    def get_handled_by_harmonic_mean_filter(self, channel):
+        pass
+
+    def get_handled_by_median_filter(self, channel, size=9):
+        pass
+
+    def change_brightness(self, channel):
+        pass
+
+
